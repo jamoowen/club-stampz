457f5cf4e944a97fa8c0ce038ca41b7384bc7e22
James Owen <jamow.owen@gmail.com>
2023-12-12 21:34:19 +0000

    changed slideover transition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -99,11 +99,11 @@ const Navbar: FC<NavbarProps> = ({ }) => {
                         <Transition.Child
                             as={Fragment}
                             enter="ease-in-out duration-500"
-                            enterFrom="opacity-100"
+                            enterFrom="opacity-0"
                             enterTo="opacity-100"
                             leave="ease-in-out duration-500"
                             leaveFrom="opacity-100"
-                            leaveTo="opacity-100"
+                            leaveTo="opacity-0"
                         >
                             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
                         </Transition.Child>
